60857ecfd8a022990e18d6c41e6c48897e54a98c
KristinaDjule <89211057@student.upr.si>
2025-05-26 23:16:55 +0200

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const cors = require("cors");
 const { register } = require("./services/auth.service");
 const { login } = require("./services/auth.service");
 const session = require("express-session");
 
 app.use(express.json());
-app.use(cors());
 
 app.use(
   session({
