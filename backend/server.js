a362956515c021dbbd9739ab7b5c245ad6803b2e
KristinaDjule <89211057@student.upr.si>
2025-05-31 10:58:13 +0200

    update authentication routes to include '/auth' prefix in server and hooks

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const { register } = require("./services/auth.service");
-const { login } = require("./services/auth.service");
 const session = require("express-session");
+const authRoutes = require("./routes/auth.routes");
 
 app.use(express.json());
 
@@ -14,6 +13,8 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
