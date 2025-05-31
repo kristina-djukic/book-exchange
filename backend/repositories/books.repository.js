d2a731fbc0e787b2579674d90dc52301537a850d
KristinaDjule <89211057@student.upr.si>
2025-05-31 12:05:20 +0200

    add books repository and createBook function, routes, and service files for book management

---
+++ backend/repositories/books.repository.js
@@ -0,0 +1,18 @@
+const db = require("../config/db.config");
+
+const createBookQuery = `
+  INSERT INTO Books (title, author, description, availability_time, user_id)
+  VALUES (?, ?, ?, ?, ?)`;
+
+const createBook = (title, author, description, availability_time, user_id) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      createBookQuery,
+      [title, author, description, availability_time, user_id],
+      (err, results) => {
+        if (err) return reject(err);
+        resolve(results.insertId);
+      }
+    );
+  });
+};
