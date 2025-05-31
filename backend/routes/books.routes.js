f2af4d79e408f0b0dadd763cd3fb9cb98d685ea0
KristinaDjule <89211057@student.upr.si>
2025-05-31 12:20:52 +0200

    add POST route for creating a book with user authentication

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -1,2 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const bookService = require("../services/book.service");
+const e = require("express");
+
+router.post("/postBook", async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    if (!userId) return res.status(401).json({ message: "Not logged in" });
+
+    const { title, author, description, availability_time } = req.body;
+    const book = await bookService.createBook(
+      title,
+      author,
+      description,
+      availability_time || null,
+      userId
+    );
+
+    res.status(201).json({ message: "Book created", book });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to create book", error: error.message });
+  }
+});
+
+module.exports = router;
