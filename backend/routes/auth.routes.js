779dad0f476db3b707682eb24fc8ef9fd5612149
KristinaDjule <89211057@student.upr.si>
2025-05-31 09:59:02 +0200

    initialize auth.routes.js file

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,3 @@
+import express from "express";
+import { register, login } from "../services/auth.service.js";
+import { Router } from "express";
--- backend/server.js
+++ backend/server.js
@@ -15,38 +15,6 @@ app.use(
   })
 );
 
-app.post("/register", async (req, res) => {
-  const { username, name, surname, email, password } = req.body;
-
-  try {
-    const user = await register(username, name, surname, email, password);
-    res.status(201).json({ message: "User registered successfully", user });
-  } catch (error) {
-    if (error.message === "EMAIL_ALREADY_EXISTS") {
-      return res.status(409).json({ message: "Email already exists" });
-    }
-    if (error.message === "USERNAME_ALREADY_EXISTS") {
-      return res.status(409).json({ message: "Username already exists" });
-    }
-    res
-      .status(400)
-      .json({ message: "Registration failed", error: error.message });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await login(email, password);
-    req.session.userId = user.id;
-    res.status(200).json({ message: "Login successful", user });
-  } catch (error) {
-    if (error.message === "INVALID_CREDENTIALS") {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
