3f8b06f861983ed4fb7a71fe43e2bf19b8dc9245
KristinaDjule <89211057@student.upr.si>
2025-06-01 20:31:48 +0200

    add getBooksByUserId function to retrieve books by user ID

--- backend/repositories/books.repository.js
+++ backend/repositories/books.repository.js
@@ -4,6 +4,8 @@ const createBookQuery = `
   INSERT INTO Books (title, author, description, availability_time, user_id)
   VALUES (?, ?, ?, ?, ?)`;
 
+const getBooksByUserIdQuery = `SELECT * FROM Books WHERE user_id = ?`;
+
 const createBook = (title, author, description, availability_time, user_id) => {
   return new Promise((resolve, reject) => {
     db.query(
@@ -16,6 +18,17 @@ const createBook = (title, author, description, availability_time, user_id) => {
     );
   });
 };
+
+const getBooksByUserId = (user_id) => {
+  return new Promise((resolve, reject) => {
+    db.query(getBooksByUserIdQuery, [user_id], (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+};
+
 module.exports = {
   createBook,
+  getBooksByUserId,
 };
